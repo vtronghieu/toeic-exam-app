9cf42de9083e3d7a950ab03ffdbe0049ff934c33
vtronghieu <vthiu002@gmail.com>
2023-06-11 17:19:30 +0700

    Upgrade controller with interface

--- src/main/java/com/tip/dg4/toeic_exam/controllers/AccountController.java
+++ src/main/java/com/tip/dg4/toeic_exam/controllers/AccountController.java
@@ -1,66 +1,26 @@
 package com.tip.dg4.toeic_exam.controllers;
 
 import com.tip.dg4.toeic_exam.common.constants.TExamApiConstant;
-import com.tip.dg4.toeic_exam.common.constants.TExamSuccessfulConstant;
 import com.tip.dg4.toeic_exam.common.responses.ResponseData;
 import com.tip.dg4.toeic_exam.dto.LoginDto;
 import com.tip.dg4.toeic_exam.dto.RegisterDto;
-import com.tip.dg4.toeic_exam.services.AccountService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping(path = TExamApiConstant.ACCOUNT_API_ROOT)
-public class AccountController {
-    private final AccountService accountService;
-
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
-
+public interface AccountController {
     @PostMapping(path = TExamApiConstant.ACCOUNT_API_LOGIN,
                  produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseData> loginAccount(@RequestBody LoginDto loginDto) {
-        HttpStatus httpStatus = HttpStatus.OK;
-        ResponseData result = new ResponseData(
-                httpStatus.value(),
-                httpStatus.getReasonPhrase(),
-                TExamSuccessfulConstant.ACCOUNT_S003,
-                accountService.loginAccount(loginDto)
-        );
-
-        return new ResponseEntity<>(result, httpStatus);
-    }
+    ResponseEntity<ResponseData> loginAccount(@RequestBody LoginDto loginDto);
 
     @PostMapping(path = TExamApiConstant.ACCOUNT_API_REGISTER,
                  produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseData> registerAccount(@RequestBody RegisterDto registerDto) {
-        HttpStatus httpStatus = HttpStatus.CREATED;
-        ResponseData result = new ResponseData(
-                httpStatus.value(),
-                httpStatus.getReasonPhrase(),
-                TExamSuccessfulConstant.ACCOUNT_S001,
-                accountService.registerAccount(registerDto)
-        );
-
-        return new ResponseEntity<>(result, httpStatus);
-    }
+    ResponseEntity<ResponseData> registerAccount(@RequestBody RegisterDto registerDto);
 
     @GetMapping(path = TExamApiConstant.API_SLASH + "{username}",
                 produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasAuthority('')")
-    public ResponseEntity<ResponseData> findByUserName(@PathVariable(name = "username") String username) {
-        HttpStatus httpStatus = HttpStatus.OK;
-        ResponseData result = new ResponseData(
-                httpStatus.value(),
-                httpStatus.getReasonPhrase(),
-                TExamSuccessfulConstant.ACCOUNT_S002,
-                accountService.findByUsername(username)
-        );
-
-        return new ResponseEntity<>(result, httpStatus);
-    }
+    @PreAuthorize("hasAuthority('admin')")
+    ResponseEntity<ResponseData> findByUserName(@PathVariable(name = "username") String username);
 }
---
+++ src/main/java/com/tip/dg4/toeic_exam/controllers/implement/AccountControllerImpl.java
@@ -0,0 +1,59 @@
+package com.tip.dg4.toeic_exam.controllers.implement;
+
+import com.tip.dg4.toeic_exam.common.constants.TExamSuccessfulConstant;
+import com.tip.dg4.toeic_exam.common.responses.ResponseData;
+import com.tip.dg4.toeic_exam.controllers.AccountController;
+import com.tip.dg4.toeic_exam.dto.LoginDto;
+import com.tip.dg4.toeic_exam.dto.RegisterDto;
+import com.tip.dg4.toeic_exam.services.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountControllerImpl implements AccountController {
+    private final AccountService accountService;
+
+    public AccountControllerImpl(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+        @Override
+    public ResponseEntity<ResponseData> loginAccount(LoginDto loginDto) {
+        HttpStatus httpStatus = HttpStatus.OK;
+        ResponseData result = new ResponseData(
+                httpStatus.value(),
+                httpStatus.getReasonPhrase(),
+                TExamSuccessfulConstant.ACCOUNT_S003,
+                accountService.loginAccount(loginDto)
+        );
+
+        return new ResponseEntity<>(result, httpStatus);
+    }
+
+    @Override
+    public ResponseEntity<ResponseData> registerAccount(RegisterDto registerDto) {
+        HttpStatus httpStatus = HttpStatus.CREATED;
+        ResponseData result = new ResponseData(
+                httpStatus.value(),
+                httpStatus.getReasonPhrase(),
+                TExamSuccessfulConstant.ACCOUNT_S001,
+                accountService.registerAccount(registerDto)
+        );
+
+        return new ResponseEntity<>(result, httpStatus);
+    }
+
+    @Override
+    public ResponseEntity<ResponseData> findByUserName(String username) {
+        HttpStatus httpStatus = HttpStatus.OK;
+        ResponseData result = new ResponseData(
+                httpStatus.value(),
+                httpStatus.getReasonPhrase(),
+                TExamSuccessfulConstant.ACCOUNT_S002,
+                accountService.findByUsername(username)
+        );
+
+        return new ResponseEntity<>(result, httpStatus);
+    }
+}
